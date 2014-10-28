48169907e98061cc21af0d0292c83458b560c360
Nikhil Kumar <nikhilkumar516@gmail.com>
2014-10-28 12:07:53 -0400

    fixed bugs

--- ResizeResume.sh
+++ ResizeResume.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dSAFER \
-  -dCompatibilityLevel="1.3" -dPDFSETTINGS="/printer" \
-  -dSubsetFonts=true -dEmbedAllFonts=true \
+  -dCompatibilityLevel="1.3" \
   -sPAPERSIZE=a4 -sOutputFile="resume.pdf" \
-  -c "<</BeginPage{1 0.97 scale 0 12.63 translate}>> setpagedevice" \
+  -c "<</BeginPage{1 0.97 scale 0 12.63 translate}>> setpagedevice " \
   -f resume1.pdf
+
+
