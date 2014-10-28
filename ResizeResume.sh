4287aa16d04a015ac11305e2c57579d4cd6fd485
Nikhil Kumar <nikhilkumar516@gmail.com>
2014-10-28 12:15:26 -0400

    added options

--- ResizeResume.sh
+++ ResizeResume.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dSAFER \
-  -dCompatibilityLevel="1.3" \
+  -dCompatibilityLevel="1.3" -dPDFSETTINGS="/printer" \
+  -dSubsetFonts=true -dEmbedAllFonts=true \
   -sPAPERSIZE=a4 -sOutputFile="resume.pdf" \
   -c "<</BeginPage{1 0.97 scale 0 12.63 translate}>> setpagedevice " \
   -f resume1.pdf
