8dffdba45ae6ce841cd5cd79ddd80bb37a75cef3
Nikhil Kumar <nikhilkumar516@gmail.com>
2014-11-19 17:38:47 -0500

    fixed script

--- ResizeResume.sh
+++ ResizeResume.sh
@@ -4,7 +4,7 @@ gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dSAFER \
   -dCompatibilityLevel="1.3" -dPDFSETTINGS="/printer" \
   -dSubsetFonts=true -dEmbedAllFonts=true \
   -sPAPERSIZE=a4 -sOutputFile="resume.pdf" \
-  -c "<</BeginPage{1 0.97 scale 0 12.63 translate}>> setpagedevice " \
+  -c "<</BeginPage{0.98 0.97 scale 11.9 12.63 translate}>> setpagedevice " \
   -f resume1.pdf
 
 
